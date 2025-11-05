11a6976b7742bd4a9d1ae0105d64dea330bd9e1e
mhndrachndraa <mhndrachndraa@gmail.com>
2025-11-05 19:00:59 +0700

    menambahkan type data

--- tugas.java
+++ tugas.java
@@ -5,5 +5,7 @@ public class tugas{
     public static void main(String[] args) {
         Scanner can = new Scanner(System.in);
         
+                float strukturData, pbo, basisData, algoritma, rpl, rataRata;
+                String predikat;
     }
 }
