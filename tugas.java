1991cef26e28f76ec96b4487963cc82bcf6309b0
mhndrachndraa <mhndrachndraa@gmail.com>
2025-11-05 20:13:59 +0700

    menambahkan total nilai dan rata rata

--- tugas.java
+++ tugas.java
@@ -5,9 +5,11 @@ public class tugas {
     public static void main(String[] args) {
         Scanner can = new Scanner(System.in);
 
-        float strukturData, pbo, basisData, algoritma, rpl, rataRata;
+        float strukturData, pbo, basisData, algoritma, rpl, rataRata, matakuliah;
         String predikat;
+        int jumlahMataKuliah = 5;
 
+        
         System.out.println("=== SISTEM SELEKSI ASISTEN DOSEN ===");
         System.out.println("Masukkan nilai Struktur Data: ");
         strukturData = can.nextFloat();
@@ -24,21 +26,20 @@ public class tugas {
         System.out.println("Masukkan nilai Rekayasa Perangkat Lunak: ");
         rpl = can.nextFloat();
 
-        rataRata = (strukturData + pbo + basisData + algoritma + rpl) / 5;
-
-        System.out.print("1. Nilai Struktur Data ="+ strukturData); 
-        can.nextDouble(); 
-
-        System.out.print("2. Nilai Pemograman Berorientasi Objek ="+ pbo); 
-        can.nextDouble(); 
-
+        System.out.print("1. Nilai Struktur Data ="+ strukturData);  
+        System.out.print("2. Nilai Pemograman Berorientasi Objek ="+ pbo);  
         System.out.print("3. Nilai Basis Data = "+ basisData); 
-        can.nextDouble(); 
-        
-        System.out.print("4. Nilai Algoritma dan Pemrograman = "+ algoritma); 
-        can.nextDouble(); 
-        
+        System.out.print("4. Nilai Algoritma dan Pemrograman = "+ algoritma);  
         System.out.print("5. Nilai RPL = "+ rpl); 
-        can.nextDouble();
+        System.out.println("Total Matkul = ");
+
+
+        float totalNilai = strukturData + pbo + basisData + basisData + algoritma;
+        rataRata = totalNilai / jumlahMataKuliah;
+
+        System.out.println("Total Nilai: " + totalNilai);
+        System.out.println("Rata-rata Nilai: " + rataRata);
+
+        
     }
 }
