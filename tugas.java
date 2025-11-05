f9a04f7deaea1c6484ab1fb975b6bf6096fbfbc0
mhndrachndraa <mhndrachndraa@gmail.com>
2025-11-05 19:07:53 +0700

    menambahkan display

--- tugas.java
+++ tugas.java
@@ -1,11 +1,28 @@
 
 import java.util.Scanner;
 
-public class tugas{
+public class tugas {
     public static void main(String[] args) {
         Scanner can = new Scanner(System.in);
-        
-                float strukturData, pbo, basisData, algoritma, rpl, rataRata;
-                String predikat;
+
+        float strukturData, pbo, basisData, algoritma, rpl, rataRata;
+        String predikat;
+
+        System.out.println("=== SISTEM SELEKSI ASISTEN DOSEN ===");
+        System.out.println("Masukkan nilai Struktur Data: ");
+        strukturData = can.nextFloat();
+
+        System.out.println("Masukkan nilai Pemrograman Berorientasi Objek: ");
+        pbo = can.nextFloat();
+
+        System.out.println("Masukkan nilai Basis Data: ");
+        basisData = can.nextFloat();
+
+        System.out.println("Masukkan nilai Algoritma dan Pemrograman: ");
+        algoritma = can.nextFloat();
+
+        System.out.println("Masukkan nilai Rekayasa Perangkat Lunak: ");
+        rpl = can.nextFloat();
+
     }
 }
