d240101617adbedafd010e8c2b70a5268dc0659f
mhndrachndraa <mhndrachndraa@gmail.com>
2025-11-05 20:26:06 +0700

    menambahkan display

--- tugas.java
+++ tugas.java
@@ -9,7 +9,6 @@ public class tugas {
         String predikat;
         int jumlahMataKuliah = 5;
 
-        
         System.out.println("=== SISTEM SELEKSI ASISTEN DOSEN ===");
         System.out.println("Masukkan nilai Struktur Data: ");
         strukturData = can.nextFloat();
@@ -26,13 +25,12 @@ public class tugas {
         System.out.println("Masukkan nilai Rekayasa Perangkat Lunak: ");
         rpl = can.nextFloat();
 
-        System.out.print("1. Nilai Struktur Data ="+ strukturData);  
-        System.out.print("2. Nilai Pemograman Berorientasi Objek ="+ pbo);  
-        System.out.print("3. Nilai Basis Data = "+ basisData); 
-        System.out.print("4. Nilai Algoritma dan Pemrograman = "+ algoritma);  
-        System.out.print("5. Nilai RPL = "+ rpl); 
+        System.out.print("1. Nilai Struktur Data =" + strukturData);
+        System.out.print("2. Nilai Pemograman Berorientasi Objek =" + pbo);
+        System.out.print("3. Nilai Basis Data = " + basisData);
+        System.out.print("4. Nilai Algoritma dan Pemrograman = " + algoritma);
+        System.out.print("5. Nilai RPL = " + rpl);
         System.out.println("Total Matkul = ");
-
 
         float totalNilai = strukturData + pbo + basisData + basisData + algoritma;
         rataRata = totalNilai / jumlahMataKuliah;
@@ -40,7 +38,7 @@ public class tugas {
         System.out.println("Total Nilai: " + totalNilai);
         System.out.println("Rata-rata Nilai: " + rataRata);
 
-         if (rataRata >= 93.75) {
+        if (rataRata >= 93.75) {
             predikat = "A (Sangat Baik)";
         } else if (rataRata >= 87.5) {
             predikat = "AB (Baik Sekali)";
@@ -50,6 +48,10 @@ public class tugas {
             predikat = "BC (Cukup)";
         } else {
             predikat = "C (Kurang)";
+
+            System.out.println("Predikat Anda: " + predikat);
+
+            can.close();
 
         }
     }
